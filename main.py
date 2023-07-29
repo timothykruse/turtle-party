18581826814c568280bd0d5a053ac078e3116c2d
timothykruse <47611537+timothykruse@users.noreply.github.com>
2023-07-29 12:25:35 -0500

    Create main.py
    Fun with turtle party functions!

---
+++ main.py
@@ -0,0 +1,18 @@
+# Turtle Party Project
+
+import turtle
+turtle.color("red")
+
+def back(len):
+  turtle.penup()
+  turtle.backward(len)
+  turtle.pendown()
+  
+def polygon(num, size):
+  for i in range(num):
+    turtle.forward(size)
+    turtle.left(360/num)
+    
+polygon(4, 100)
+back(125)
+polygon(3, 50)
